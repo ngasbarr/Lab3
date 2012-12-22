c3b56cfa4699bb3d8598bf3df2a2bc846b1d4a66
NickG <ngasbarr@andrew.cmu.edu>
2012-12-21 23:39:59 -0500

    to finish tomorrow

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,20 +1,19 @@
-def convert(temp,measure)
-return "Temperature must be an integer" unless temp.class == Fixnum
-return "Temperature below absolute zero" if temp < -474
+def convert(temp,measure="F")
+	return "Temperature must be an integer" unless temp.class == Fixnum
+	return "Temperature below absolute zero" if temp < -474
 
-if measure == "F"
-	5*(temp-32)/9
-else if measure == "C"
-	9*(temp-32)/5
-else
-	return "Conversion error"
-end
-end
+	if measure == "F"
+		5*(temp-32)/9
+	elsif measure == "C"
+		(9*(temp)/5)+32
+	else
+		return "Conversion error"
+	end
 end
 
 puts convert(32, "C")
 puts convert(50, "F")
-puts convert(15,"C")
+puts convert(15)
 puts convert(-500, "F")
 
 puts convert(0, "C")
