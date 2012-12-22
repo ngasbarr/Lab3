c5ad24fd0efc114b2fb17c08902c5b528769168c
NickG <ngasbarr@andrew.cmu.edu>
2012-12-21 22:44:30 -0500

    added test statements included in spec

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -16,3 +16,8 @@ puts convert(32, "C")
 puts convert(50, "F")
 puts convert(15,"C")
 puts convert(-500, "F")
+
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
