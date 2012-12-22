844d1174f4b79d20629b82d0d6e0a45cbbd5606e
NickG <ngasbarr@andrew.cmu.edu>
2012-12-22 13:55:38 -0500

    added below_abs_zero method

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,6 +1,6 @@
 def convert(temp,measure="F")
 	return "Temperature must be an integer" unless temp.class == Fixnum
-	return "Temperature below absolute zero" if temp < -474
+	return "Temperature below absolute zero" if below_absolute_zero(temp,measure)==true
 
 	if measure == "F"
 		5*(temp-32)/9
@@ -10,6 +10,13 @@ def convert(temp,measure="F")
 		return "Conversion error"
 	end
 end
+
+def below_absolute_zero(temp, measure)
+	return false unless (temp < -454 and measure =="F") or (temp < -270 and measure =="C")
+	
+	true
+end
+
 
 puts convert(32, "C")
 puts convert(50, "F")
